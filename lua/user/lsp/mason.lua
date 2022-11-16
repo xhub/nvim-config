3e539e31c7b213aa6a9e64f628af18fbabc143ed
Olivier Huber <oli.huber@gmail.com>
2022-11-16 09:34:50 +0100

    [compat] change update_capabilities -> default_capabilities

--- lua/user/lsp/mason.lua
+++ lua/user/lsp/mason.lua
@@ -5,9 +5,7 @@ local lsp_defaults = {
   flags = {
     debounce_text_changes = 150,
   },
-  capabilities = require('cmp_nvim_lsp').update_capabilities(
-    vim.lsp.protocol.make_client_capabilities()
-  ),
+  capabilities = require('cmp_nvim_lsp').default_capabilities(),
   on_attach = require("user.lsp.handlers").on_attach,
 }
 
