b86ccb8a0c1d3d889e44a63fb0bd613770365b51
Olivier Huber <oli.huber@gmail.com>
2023-01-13 13:40:15 +0100

    add filetypes

---
+++ lua/user/filetypes.lua
@@ -0,0 +1,7 @@
+-- custom filetypes
+
+-- this is for the include files in reshop
+vim.cmd [[ autocmd BufRead,BufNewFile *.inc set filetype=c ]]
+
+vim.cmd [[ autocmd BufRead,BufNewFile *.gms set filetype=gams ]]
+
