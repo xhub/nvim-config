9c56c8f2a938fa4347f4179530c9dde6759a15a6
Olivier Huber <oli.huber@gmail.com>
2023-07-28 11:32:45 -0500

    remove grammar_guard, add sioyek

--- lua/user/xhub.lua
+++ lua/user/xhub.lua
@@ -1,32 +1,35 @@
--- hook to nvim-lspconfig
-require("grammar-guard").init()
-
--- setup LSP config
-require("lspconfig").grammar_guard.setup({
-  cmd = { vim.fn.getenv("HOME") ..'/.local/share/nvim/mason/bin/ltex-ls' }, -- add this if you install ltex-ls yourself
-  settings = {
-    ltex = {
-      enabled = { "latex", "tex", "bib", "markdown", "text" },
-      language = "en",
-      -- diagnosticSeverity = "information",
-      dignosticSeverity = "hint",
-      sentenceCacheSize = 2000,
-      additionalRules = {
-        enablePickyRules = true,
-        motherTongue = "fr",
-      },
-      trace = { server = "verbose" },
-      dictionary = { ["en-US"] = { ":" .. vim.fn.getenv("HOME") .. "/.vim-spell-en.utf-8.add" } },
-      disabledRules = {},
-      hiddenFalsePositives = {},
-    },
-  },
-})
+-- -- hook to nvim-lspconfig
+-- require("grammar-guard").init()
+-- 
+-- -- setup LSP config
+-- require("lspconfig").grammar_guard.setup({
+--   cmd = { vim.fn.getenv("HOME") ..'/.local/share/nvim/mason/bin/ltex-ls' }, -- add this if you install ltex-ls yourself
+--   settings = {
+--     ltex = {
+-- --      enabled = { "latex", "tex", "bib", "markdown", "text" },
+--       enabled = { "bib", "markdown", "text" },
+--       language = "en",
+--       -- diagnosticSeverity = "information",
+--       dignosticSeverity = "hint",
+--       sentenceCacheSize = 2000,
+--       additionalRules = {
+--         enablePickyRules = true,
+--         motherTongue = "fr",
+--       },
+--       trace = { server = "verbose" },
+--       dictionary = { ["en-US"] = { ":" .. vim.fn.getenv("HOME") .. "/.vim-spell-en.utf-8.add" } },
+--       disabledRules = {},
+--       hiddenFalsePositives = {},
+--     },
+--   },
+-- })
 
 
 require('goto-preview').setup({
   default_mappings = true;
 })
+
+vim.g.vimtex_view_method = 'sioyek'
 
 -- local lspconfig = require("lspconfig")
 
